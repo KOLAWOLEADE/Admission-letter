301fbf46c1b0d2427e76a29f20645aacdb29d27d
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-06-23 22:48:08 -0700

    add local

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    $(".letter").submit(function(){
+    $("#letter").submit(function(event){
+        const houseInput = $("#house").val();
+        const localInput = $("#local").val ();
+        const stateInput  = $("#State") .val();
+        const countryInput = $("#Country").val();
+        const dateInput    = $("#date").val();
+
+
+        $(".house").text(houseInput);
+        $(".loacl").text(localInput);
+        $(".state").text(stateInput);
+        $(".country").text(countryInput);
+        $(".date").text(dateInput);
+
+
+        $("#official").show();
+       
         
-    })
-})
+
+  });
+});
